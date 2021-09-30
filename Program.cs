4da52cc0c1df8a1b9697f843991a24b56cd021cb
David <DavidM>
2021-09-30 16:32:05 +0200

    prvi commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Property_Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
