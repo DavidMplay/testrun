d5e585a27cb2f974150bb917c3563e80a14dcdd4
David <DavidM>
2021-09-30 17:16:39 +0200

    Added private string name

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ class Program
     {
         static void Main(string[] args)
         {
-        Ucenik David = new Ucenik();
+        Ucenik David = new Ucenik("David");
         }
     }
--- Ucenik.cs
+++ Ucenik.cs
@@ -2,8 +2,10 @@
 
 class Ucenik
 {
-    public Ucenik()
+    private string _name;
+    public Ucenik(string name)
     {
-        Console.WriteLine("New object generated");
+        this._name = name;
+        Console.WriteLine(_name);
     }
 }
